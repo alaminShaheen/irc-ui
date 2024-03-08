731bdcb0b0e67f86bc309146e4181a89372d5ee4
alaminShaheen <As2022!!@@>
2024-03-08 13:54:54 -0500

    Initialized tailwind theming for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 /** @type {import("tailwindcss").Config} */
 
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#093C55",
+        "secondary": "#F5F5F5"
+      }
+    }
+  },
+  plugins: []
 };
 
