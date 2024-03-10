ad41e133059d1a0e7ff7f8957e8de72d00ae3ded
alaminShaheen <As2022!!@@>
2024-03-10 01:25:11 -0500

    Fixed checkbox bug

--- src/components/ui/Checkbox/Checkbox.tsx
+++ src/components/ui/Checkbox/Checkbox.tsx
@@ -4,9 +4,11 @@ import { ICheckboxProps } from "@/components/ui/Checkbox/Checkbox.d";
 
 const Checkbox = (props: ICheckboxProps) => {
   const { className, ...rest } = props;
+
   return (
     <input {...rest} type="checkbox"
-           className={twMerge(className, "checkbox rounded w-[30px] h-[30px] border-primary [--chkbg:theme(colors.primary)] [--chkfg:white]")} />
+           className={twMerge(className, "checkbox rounded w-[30px] h-[30px] border-primary [--chkbg:theme(colors.primary.900)] [--chkfg:white]")} />
+
   );
 };
 
