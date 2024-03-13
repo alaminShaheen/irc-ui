2171bf62a110e551fa831b52946100c4db22f94b
alaminShaheen <As2022!!@@>
2024-03-12 23:53:27 -0400

    Separated translation files into en.json and fr.json

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,13 @@
 import i18n from "i18next";
-import LanguageDetectorModule from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import LanguageDetectorModule from "i18next-browser-languagedetector";
+
 import { LanguageCode } from "@/models/enums/LanguageCode";
+import en from "@/locales/en.json";
+import fr from "@/locales/fr.json";
 
 i18n.use(LanguageDetectorModule).use(initReactI18next).init({
-  debug: !process.env.NODE_ENV || process.env.NODE_ENV === 'development',
+  debug: !process.env.NODE_ENV || process.env.NODE_ENV === "development",
   fallbackLng: ["en", "fr"],
   supportedLngs: Object.values(LanguageCode),
   detection: {
@@ -12,14 +15,10 @@ i18n.use(LanguageDetectorModule).use(initReactI18next).init({
   },
   resources: {
     en: {
-      translation: {
-        "your-policies": "Your Policies",
-      },
+      translation: en,
     },
     fr: {
-      translation: {
-        "your-policies": "Vos Politiques",
-      },
+      translation: fr,
     },
   },
 });
