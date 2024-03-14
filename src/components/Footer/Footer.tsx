f5918da3ba011f4912e72d850ce1dba2559d910c
Farzana Akter Shampa <61857690+farzanashampa7@users.noreply.github.com>
2024-03-14 13:25:15 -0400

    Added both language on footer static content, added separate icons for each social and fixed the focustrap issue in SideBar component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,21 @@
+import { useTranslation } from "react-i18next";
+
+import { FaFacebookF } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+
 import socialMedia from "@/assets/socialMedia.svg";
 import logo from "@/assets/logo.svg";
 
 const Footer = () => {
+  const { t } = useTranslation();
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer id="footer-content" className="bg-primary text-white font-Roboto">
-      <div
-        className="pt-[51px] max-sm:px-4 sm:max-md:px-8 max-sm:pb-[62px] sm:max-md:pb-[67px] md:pt-[92px] md:pb-[54px]">
+      <div className="pt-[51px] max-sm:px-4 sm:max-md:px-8 max-sm:pb-[62px] sm:max-md:pb-[67px] md:pt-[92px] md:pb-[54px]">
         <p className="text-sm text-left text-primary-25 font-light md:ml-[156px]">
-          Powered by:
+          {t("common.poweredBy")}
         </p>
         <img
           className="w-[302px] h-[39px] md:w-[377px] md:h-[49px] mt-1 md:ml-[151px]"
@@ -16,13 +24,9 @@ const Footer = () => {
         />
 
         <p className="text-sm md:text-lg mt-5 lg:mt-4 md:ml-[156px] md:mr-[152px] text-start font-normal">
-          Instant Risk Coverage is a trademark of Instant Risk Coverage Inc.
-          Refer to your policy for the most detailed and accurate information
-          about your coverage and terms of insurance. Your policy, which serves
-          as your insurance contract, will always prevail if there's ever a
-          conflict with the information found on this site.
+          {t("common.footerBody")}
         </p>
-        <div className="flex justify-start md:justify-end mt-[47.5px] md:mr-[130px]">
+        {/* <div className="flex justify-start md:justify-end mt-[47.5px] md:mr-[130px]">
           <a href="" target="_blank">
             <img
               className="w-[128px] h-[33.49px]"
@@ -30,8 +34,28 @@ const Footer = () => {
               alt="Social Media"
             />
           </a>
+        </div> */}
+        <div className="flex justify-start md:justify-end mt-[47.5px] md:mr-[130px]">
+          <div className="flex justify-evenly w-28">
+            <a href="#" target="_blank">
+              <div className="bg-white text-primary w-6 h-6 flex justify-center items-end rounded-full">
+                <FaFacebookF size={20} />
+              </div>
+            </a>
+            <a href="#" target="_blank">
+              <div className="bg-white text-graphite-600 w-6 h-6 flex justify-center items-center rounded-full">
+                <FaInstagram />
+              </div>
+            </a>
+            <a href="#" target="_blank">
+              <div className="bg-white text-graphite-600 w-6 h-6 flex justify-center items-center rounded-full">
+                <FaXTwitter />
+              </div>
+            </a>
+          </div>
         </div>
-        <div className="mt-[38.5px] md:mt-3 md:ml-[134px] md:mr-[130px] ">
+
+        <div className="mt-7 md:mt-3 md:ml-[134px] md:mr-[130px] ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 1176 1"
@@ -48,10 +72,10 @@ const Footer = () => {
             href=""
             target="_blank"
           >
-            Privacy policy
+            {t("common.privacyPolicy")}
           </a>
           <p className="mt-[27px] md:mt-[33px] text-base font-medium">
-            © {new Date().getFullYear()} instantriskcoverage
+            {t("common.copyright", { year: currentYear })}
           </p>
         </div>
       </div>
