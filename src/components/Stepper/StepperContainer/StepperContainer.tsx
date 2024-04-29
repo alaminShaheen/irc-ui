e8749014cca636c749993dcf2a103075108ed9fe
Alamin Shaheen <43922737+alaminShaheen@users.noreply.github.com>
2024-04-29 03:31:02 -0400

    chore: fixed some responsive issues

--- src/components/Stepper/StepperContainer/StepperContainer.tsx
+++ src/components/Stepper/StepperContainer/StepperContainer.tsx
@@ -6,8 +6,8 @@ import { StepperContextProvider } from "@/context/StepperContext";
 const StepperContainer = () => {
   return (
     <StepperContextProvider>
-      <div className="mx-auto grid h-full w-11/12 grid-rows-[98px_1fr] lg:grid-cols-[1fr_600px] lg:grid-rows-1">
-        <div className="order-2 px-4 py-6 lg:order-1 lg:px-9 lg:py-8">
+      <div className="mx-auto grid h-full w-full grid-rows-[78px_1fr] lg:w-11/12 lg:grid-cols-[1fr_600px] lg:grid-rows-1">
+        <div className="order-2 lg:order-1 lg:my-0 lg:px-9 lg:py-8">
           <Outlet />
         </div>
         <StepperSidebar />
